e16597ed7bd094ab9b8172366e066a93460876ca
Radosław Ziomber <rziomber@op.pl>
2023-02-28 05:38:45 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::string pythonCode = R"rawliteral(
+    const char *pythonCode = R"rawliteral(
 import time
 print('Hello World from Embedded Python!!!')
 def getFive():
@@ -14,7 +14,7 @@ def getProduct(a,b):
    return a*b
 )rawliteral";
 
-    PyRun_SimpleString(pythonCode.c_str());
+    PyRun_SimpleString(pythonCode);
 
     int result = py.runPythonFunctionLong("__main__", "getFive");
     double result2 = py.runPythonFunctionDouble("__main__", "getProduct", 2.8, 4.2);
