962b3f19d734b1243a495a0dbbbb8c42c3b9e8a1
Radosław Ziomber <rziomber@op.pl>
2023-05-16 17:12:24 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,18 +10,24 @@ import time
 print('Hello World from Embedded Python!!!')
 def getFive():
    return 5
+def getString():
+   return "string"
 def getProduct(a,b):
    return a*b
 )rawliteral";
 
     PyRun_SimpleString(pythonCode);
 
-    int result = py.runPythonFunctionLong("__main__", "getFive");
-    double result2 = py.runPythonFunctionDouble("__main__", "getProduct", 2.8, 4.2);
+    int result = py.runPythonFunction<long>("__main__", "getFive");
+    double result2 = py.runPythonFunction<double>("__main__", "getProduct", 2.1, 4);
     std::string result3 = py.runPythonFunction("time.time()");
+    long result4 = py.runPythonFunction<long>("__main__", "getProduct", 2, 4);
+    const char* result5 = py.runPythonFunction<const char*>("__main__", "getString");
     std::cout << result << "\n"
               << result2 << "\n"
-              << result3 << "\n";
+              << result3 << "\n"
+              << result4 << "\n"
+              << result5 << "\n";
 
     return 0;
 }
